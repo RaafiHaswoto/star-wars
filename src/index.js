7942ce8095cf0e2ea92e6bece52a852c3db37675
raafihaswoto <raafi@aleph-labs.com>
2020-11-14 14:43:38 +0700

    added some styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+// Polyfill IE
+import 'es5-shim';
+import 'es5-shim/es5-sham';
+import 'core-js/es/map';
+import 'core-js/es/set';
+import 'raf/polyfill';
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// Main Style
+import './assets/styles/main.scss';
+
+// App
+import App from './container/App';
+
+// Report
+import reportWebVitals from './report/reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
